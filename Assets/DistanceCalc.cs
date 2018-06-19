f68724f6d053bcc7c7b6da05cb243be6955d577c
Unknown <garydoesthings@gmail.com>
2018-06-18 20:46:18 -0600

    Submission
    Uploading files for submission

---
+++ Assets/DistanceCalc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceCalc : MonoBehaviour {
+
+    float distanceTravelled = 0;
+    Vector3 lastPosition;
+
+    // Use this for initialization
+    void Start () {
+        lastPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distanceTravelled += Vector3.Distance(transform.position, lastPosition);
+        lastPosition = transform.position;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Distance traveled: "+distanceTravelled.ToString());
+    }
+}
